b6b5a1747ff1b43f21a586bf0a360d89a1657a9a
godicheol <godicheol@gmail.com>
2023-05-31 22:33:26 +0900

    fix

--- forge.config.js
+++ forge.config.js
@@ -6,7 +6,7 @@ module.exports = {
     executableName: "clipboard-translator",
     asar: false,
     appBundleId: "com.godicheol.clipboardtranslator",
-    icon: "./assets/icons/icon",
+    icon: path.resolve(__dirname, "./assets/icons/icon"),
   },
   rebuildConfig: {},
   makers: [
